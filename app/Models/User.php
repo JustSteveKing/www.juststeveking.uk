37f2b2df5383dd5f16a522864630e49952286e4f
Steve McDougall <juststevemcd@gmail.com>
2023-11-08 20:03:14 +0000

    User Model changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,45 +1,51 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Carbon\CarbonInterface;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+/**
+ * @property string $id
+ * @property string $name
+ * @property null|string $role
+ * @property null|string $company
+ * @property null|string $avatar
+ * @property string $email
+ * @property string $password
+ * @property null|string $remember_token
+ * @property null|CarbonInterface $created_at
+ * @property null|CarbonInterface $updated_at
+ */
+final class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use HasUlids;
+    use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
+        'role',
+        'company',
+        'avatar',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
+
     protected $casts = [
-        'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
 }
