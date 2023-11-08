ebd1114f29e09cde34cdf74d8487aa7751c8f367
Steve McDougall <juststevemcd@gmail.com>
2023-11-08 20:09:48 +0000

    Setting up developer tooling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,10 +9,7 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 final class Kernel extends ConsoleKernel
 {
-    protected function schedule(Schedule $schedule): void
-    {
-        //
-    }
+    protected function schedule(Schedule $schedule): void {}
 
     protected function commands(): void
     {
