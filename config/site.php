bf5c630a54cd00e1d8f28c8d08b48da8a5cefa94
Steve McDougall <juststevemcd@gmail.com>
2023-11-08 21:30:46 +0000

    Setting everything up

---
+++ config/site.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'title' => 'JustSteveKing',
+    'subtitle' => 'Welcome to my website, the realm of the API Guy. Led by a seasoned Consultant CTO, Software Engineer, Developer Advocate, and renowned Conference Speaker',
+];
